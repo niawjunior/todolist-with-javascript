e96be1abda9dba4c79727e794b9bd2f09f5dfeac
niawkung <niawkung2@gmail.com>
2017-07-29 20:33:41 +0700

    create show data function

--- myscript.js
+++ myscript.js
@@ -33,3 +33,23 @@ function remove() {
 
     return false;
 }
+
+
+function show() {
+    var todos = get_todos();
+
+    var html = '<ul>';
+    for(var i=0; i<todos.length;i++){
+        html +='<li>' + todos[i] + '<button class="remove" id="' + i + '">Delete</button> </li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i< buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click',add);
+show();
